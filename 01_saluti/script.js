2673b7fddd7695121f1b711b9831b9ab91bc8c5a
Filippo Graziano <filippograziano13@gmail.com>
2026-05-20 14:31:39 +0200

    add folders with index and sript

---
+++ 01_saluti/script.js
@@ -0,0 +1,16 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+
+// Dichiara la funzione qui.
+function saluto(name) {
+    console.log(`Ciao ${name}`);
+    return;
+};
+
+// Invoca la funzione qui e stampa il risultato in console
+saluto(`Mario`)
+
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
