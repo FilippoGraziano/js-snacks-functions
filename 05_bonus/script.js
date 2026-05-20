70a7c3b1d42bb1bce91106a92f97bb1bab7714db
Filippo Graziano <filippograziano13@gmail.com>
2026-05-20 23:33:46 +0200

    complete fifth exercise

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,27 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function _saluto(name) {
+    const now = new Date();
+    const hours = now.getHours();
 
+    if (hours <= 13) return console.log(`Buongiorno ${name}`);
+    if (hours > 13 && hours <= 17) return console.log(`Buonpomeriggio ${name}`);
+    if (hours > 17) return console.log(`Buonasera ${name}`);
+    return console.log(undefined);
+};
 
+const saluto = name => {
+    const now = new Date();
+    const hours = now.getHours();
+
+    if (hours <= 13) return console.log(`Buongiorno ${name}`);
+    if (hours > 13 && hours <= 17) return console.log(`Buonpomeriggio ${name}`);
+    if (hours > 17) return console.log(`Buonasera ${name}`);
+    return console.log(undefined);
+}
 // Invoca la funzione qui e stampa il risultato in console
 
-
+saluto(`Mario`)
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
