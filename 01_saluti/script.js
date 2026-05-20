742861da323df31780966daf63894b46c182a086
Filippo Graziano <filippograziano13@gmail.com>
2026-05-20 14:34:31 +0200

    complete first exercise

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,13 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-function saluto(name) {
-    console.log(`Ciao ${name}`);
-    return;
-};
+
+// function saluto(name) {
+//     console.log(`Ciao ${name}`);
+//     return;
+// };
+
+const saluto = name => console.log(`Ciao ${name}`)
 
 // Invoca la funzione qui e stampa il risultato in console
 saluto(`Mario`)
